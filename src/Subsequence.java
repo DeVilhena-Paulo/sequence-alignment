28022b524b6987cdc76e0de209835664deae38a6
Paulo Emilio de Vilhena <pevilhena2@gmail.com>
2017-01-05 03:17:46 +0100

    Completing class TestSubsequence

--- src/Subsequence.java
+++ src/Subsequence.java
@@ -47,7 +47,6 @@ public class Subsequence {
                 }
             }
         }
-
         return path;
     }
 
--- src/TestSubsequence.java
+++ src/TestSubsequence.java
@@ -1,7 +1,30 @@
-package PACKAGE_NAME;
+import java.io.IOException;
 
-/**
- * Created by Paulo on 05/01/2017.
- */
 public class TestSubsequence {
+
+    public static void main (String[] arg) throws IOException {
+
+        String result;
+
+        //Test 1
+        result = Subsequence.lcs("SubSeq_Res52.txt");
+        if (result.length() != 52)
+            System.out.println ("SubSeq_Rec52: FAILED");
+        else
+            System.out.println ("SubSeq_Rec52: PASSED");
+
+        //Test 2
+        result = Subsequence.lcs("SubSeq_Res320.txt");
+        if (result.length() != 320)
+            System.out.println ("SubSeq_Rec320: FAILED");
+        else
+            System.out.println ("SubSeq_Rec320: PASSED");
+
+        //Test 3
+        result = Subsequence.lcs("SubSeq_Res3275.txt");
+        if (result.length() != 3275)
+            System.out.println ("SubSeq_Rec3275: FAILED");
+        else
+            System.out.println ("SubSeq_Rec3275: PASSED");
+    }
 }
